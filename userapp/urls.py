159779c7a1f9eeabaf38c32d6cda9f23e318bdca
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2020-12-27 15:48:59 +0530

    Created templates for login,signup,cart using bootstrap

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
-    #path('', TemplateView.as_view(template_name='blog.html')),
-    #path('loginlog/',views.loginlog),
+    path('home',views.home,name='home'),
+    path('login_form',views.login_form,name='login'),
+    path('signup_form',views.signup_form,name='signup_form'),
+    path('cart',views.cart,name='cart'),
+
     #path('logout/',views.logout),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'base.html')
+    return render(request,'home.html')
+
+def login_form(request):
+    return render(request, 'login.html')
+
+def signup_form(request):
+    return render(request, 'signup.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
