8213bfd14629fc6829bb42fa8285ae4a6ba68ad7
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2021-01-09 10:50:54 +0530

    Heroku configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3k!aa$($*l8dakus2c)w6jn98qjaxch12)(g2h)x^!88+#!)+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
