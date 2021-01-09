fb5a76ac2412d72955002db4c416879f0c899bb9
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2021-01-09 10:33:09 +0530

    hot fix in email

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kevinjoseminj@gmail.com'
-EMAIL_HOST_PASSWORD = 'realmekevin'
+EMAIL_HOST_USER = 'buildingmaterials213@gmail.com'
+EMAIL_HOST_PASSWORD = 'materials@123'
