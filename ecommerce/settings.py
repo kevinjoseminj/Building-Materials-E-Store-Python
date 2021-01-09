0d8ad87908e0c42a433583783e5dc6743d09d215
Kevin Jose Minj <kevinjoseminj98@gmail.com>
2021-01-09 18:21:49 +0530

    backend db changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'PASSWORD':'root@123',
-        'NAME': 'ecommerce',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -27,7 +26,7 @@ urlpatterns = [
     path('', include('userapp.urls')),
     path('admin/', admin.site.urls),
     path('', include('adminapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
